5de5601e85bc7b801633218114583dce573153fd
Luhmeiy <medeiros.luizhenrique@yahoo.com.br>
2025-11-03 09:08:07 -0300

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import 'uno.css';
+	import '@unocss/reset/tailwind.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<title>Tile Flip</title>
+</svelte:head>
+
+<div>
+	{@render children()}
+</div>
