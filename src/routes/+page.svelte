d3c72684695d1b23d4bcdeb64aa1bc314cfee7e4
Luhmeiy <medeiros.luizhenrique@yahoo.com.br>
2025-11-08 16:01:15 -0300

    feat(game-logic): add startGame function

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import { Button, ButtonGroup, Grid } from '$lib';
 
 	const [rows, cols] = [5, 5];
@@ -7,8 +8,46 @@
 		Array.from({ length: rows }, () => Array.from({ length: cols }, () => true))
 	);
 
+	let isGameStarted = $state(false);
 	let isGameWon = $state(false);
+
 	let flipDirection = $state('col');
+	let moves = $state(0);
+
+	const startGame = () => {
+		isGameStarted = false;
+
+		const [min, max] = [4, 8];
+
+		moves = Math.floor(Math.random() * (max - min + 1) + min);
+
+		let usedDirections: string[] = [];
+		let usedPosition: (number | undefined)[] = [];
+
+		while (moves >= 0) {
+			const direction = ['col', 'row', 'diagonal'][Math.floor(Math.random() * 3)];
+
+			let position;
+
+			if (direction === 'diagonal' && usedDirections.at(-1) === 'diagonal') continue;
+
+			if (direction === 'col') {
+				position = Math.floor(Math.random() * cols);
+			} else if (direction === 'row') {
+				position = Math.floor(Math.random() * rows);
+			}
+
+			if (direction === usedDirections.at(-1) && position === usedPosition.at(-1)) continue;
+
+			flipTiles({ direction, position });
+
+			usedDirections.push(direction);
+			usedPosition.push(position);
+			moves--;
+		}
+
+		isGameStarted = true;
+	};
 
 	const flipTiles = ({ direction, position }: { direction: string; position?: number }) => {
 		if (typeof position === 'number') {
@@ -36,20 +75,24 @@
 	$effect(() => {
 		if (isGameWon) console.log('You win');
 	});
+
+	onMount(() => startGame());
 </script>
 
 <div class="flex">
-	<div class="flex flex-col gap-1">
-		<ButtonGroup length={rows} direction="row" {flipTiles} />
+	{#if isGameStarted}
+		<div class="flex flex-col gap-1">
+			<ButtonGroup length={rows} direction="row" {flipTiles} />
 
-		<Button flipTiles={() => flipTiles({ direction: 'diagonal' })} />
-	</div>
-
-	<div>
-		<Grid {grid} {flipDirection} />
-
-		<div class="flex gap-1">
-			<ButtonGroup length={cols} direction="col" {flipTiles} />
+			<Button flipTiles={() => flipTiles({ direction: 'diagonal' })} />
 		</div>
-	</div>
+
+		<div>
+			<Grid {grid} {flipDirection} />
+
+			<div class="flex gap-1">
+				<ButtonGroup length={cols} direction="col" {flipTiles} />
+			</div>
+		</div>
+	{/if}
 </div>
